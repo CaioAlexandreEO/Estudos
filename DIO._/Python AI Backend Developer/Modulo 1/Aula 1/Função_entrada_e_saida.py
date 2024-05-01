10e72ed0394f8765870077b74d9715eae5c7faa5
CaioAlexandreEO <168662068+CaioAlexandreEO@users.noreply.github.com>
2024-05-01 14:01:44 -0300

    Update Função_entrada_e_saida.py

--- DIO._/Python AI Backend Developer/Modulo 1/Aula 1/Função_entrada_e_saida.py
+++ DIO._/Python AI Backend Developer/Modulo 1/Aula 1/Função_entrada_e_saida.py
@@ -1,1 +1,13 @@
-nome=input
+nome=input("Informe o seu nome: ")
+print("Seu nome é", nome, ", ja está cadastrado!")
+
+
+________________________________________________________________
+
+
+nome=input("Informe seu nome: ")
+idade=input("Informe sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end=("...\n")
+print(nome, idade, end=(" ")
