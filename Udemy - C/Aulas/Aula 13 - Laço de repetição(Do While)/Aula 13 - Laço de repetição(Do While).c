c87f198d8118960ca2da9c1e1ddb6816e3bcde31
Caio Alexandre <168662068+CaioAlexandreEO@users.noreply.github.com>
2025-03-20 13:27:16 -0300

    Create Aula 13 - Laço de repetição(Do While).c

---
+++ Udemy - C/Aulas/Aula 13 - Laço de repetição(Do While)/Aula 13 - Laço de repetição(Do While).c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+    int a = 1, b = 10;
+
+    //Primeiro executa uma vez e depois confere a condi��o, ao contrario do while
+    do{
+
+
+        //Imprimindo as reps
+        printf("\n%d", a);
+
+        //Incrementar a+1 at� 10
+        a++;
+    }//Contando at� 10
+    while(a<=10);
+
+
+
+
+
+system("pause");
+}
