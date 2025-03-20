66f2bc50f8588b128748321fc8ea95fc2c6548f2
Caio Alexandre <168662068+CaioAlexandreEO@users.noreply.github.com>
2025-03-20 13:43:12 -0300

    Create Aula 22 - Funções inteiras- C++.cpp

---
+++ Udemy - C/Aulas/Aula 22 - Funções inteiras- C++/Aula 22 - Funções inteiras- C++.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Definindo que a fun��o existe
+int retornaDez();
+float retornaQuebrado();
+
+int main(){
+
+    //Definindo Vari�veis
+    int a;
+
+    //Passando o retorno de uma fun��o para uma vari�vel
+    a = retornaDez();
+
+    //Imprimindo valor de a
+    printf("\n%d", a);
+
+    //Definndo um float
+    float b;
+
+    //Passando o retorno de uma fun��o para uma vari�vel
+    b = retornaQuebrado();
+
+    //Imprimindo valor de a
+    printf("\n%.2f", b);
+
+    return 0;
+}
+
+//Fun��o que retorna 10
+int retornaDez(){
+    printf("\nOi galera! \n");
+    int numero = 10;
+    return numero;
+}
+
+//Retorna um n�mero quebrado
+float retornaQuebrado(){
+    return 5.5;
+}
+
